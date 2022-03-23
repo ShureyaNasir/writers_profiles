f7b583b13beed53edada61deda8f802e1a44a3f2
ShureyaNasir <shureyanasir1994@gmail.com>
2022-03-23 11:48:00 +0000

    react events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ProfileCard from './component/ProfileCard'
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.handleClick =this.handleClick.bind(this);
+    this.state ={
+      writers:{
+        loading: false,
+        list: []
+      }
+    };
+  }
+  handleClick() {
+    this.setState({
+      writers:{
+        loading:true
+      }
+    });
+
+    setTimeout(async()=>{
+      let resp = await fetch("/writers.json");
+      let result = await resp.json();
+
+      this.setState({
+        writers:{
+          loading:false,
+          list: result
+        }
+      });
+    }, 3500);
+  }
+  render() {
+    const {
+      writers:{loading, list}
+    } = this.state;
+
+    if(loading) {
+      return (
+        <div>
+        <h1>Writers Profile</h1>
+        <div className='container'>
+        <div className='card action'>
+        <p className='infoText'>Loading...</p>
+        </div>
+        </div>
+        </div>
+      )
+    }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+    <h1>Writers Profile</h1>
+    <div className='container'>
+    {list.length === 0 ? (
+      <div className='card action'>
+      <p className='infotext'>Ooops....no writer profile found</p>
+      <button className='actionBtn' onClick={this.handleClick}> Get Writers</button>
+      </div>
 
-export default App;
+    ):(
+      lsit.map((writer) =>(
+        <ProfileCard key={writer.id} writer={writer}/>
+      ))
+    )}
+    </div>
+    </div>
+  )
+      }
+    }
+      
